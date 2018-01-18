a49eeab8844bebcd32c4ed2656286c804a7efaa2
Jared Brook <j.brook@base2services.com>
2018-01-19 08:56:24 +1100

    Allow additional number classes in mappings rendering

--- ext/alarms.rb
+++ ext/alarms.rb
@@ -28,10 +28,10 @@ def create_param_mappings(params,template_envs,alarmHash)
   mappings = {}
   params.each do |key,value|
     if !key.include? '.'
-      if [String, Integer, TrueClass].member?(value.class)
+      if [String, Integer, Float, Fixnum, TrueClass].member?(value.class)
         mappings[key] = {}
         template_envs.each do |env|
-          if !params["#{key}.#{env}"].nil? && [String, Integer, TrueClass].member?(params["#{key}.#{env}"].class)
+          if !params["#{key}.#{env}"].nil? && [String, Integer, Float, Fixnum, TrueClass].member?(params["#{key}.#{env}"].class)
             mappings[key][env] = params["#{key}.#{env}"]
           else
             mappings[key][env] = value
