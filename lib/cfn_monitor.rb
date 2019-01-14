5dc867b4e13fbf01c19dfe656c149ab2d0d7ad02
zac <z.lewis@base2services.com>
2019-01-14 13:14:49 +1100

    Add option to validate CloudFormation templates.

--- lib/cfn_monitor.rb
+++ lib/cfn_monitor.rb
@@ -2,6 +2,7 @@ require "thor"
 require "cfn_monitor/version"
 require "cfn_monitor/query"
 require "cfn_monitor/generate"
+require "cfn_monitor/validate"
 require "cfn_monitor/deploy"
 
 module CfnMonitor
@@ -17,7 +18,7 @@ module CfnMonitor
         aliases: :s,
         type: :boolean,
         default: false,
-        desc: "Don't print cfndsl output"
+        desc: "Don't print Cfndsl output"
 
     # class_option :verbose,
     #   aliases: :V,
@@ -38,9 +39,9 @@ module CfnMonitor
     #   type: :string,
     #   desc: "Profile name in AWS credentials file"
 
-    desc "generate", "Generate monitoring cloudformation templates"
+    desc "generate", "Generate monitoring CloudFormation templates"
     long_desc <<-LONG
-    Generates cloudformation templates from the alarm configuration and output to the output/ directory.
+    Generates CloudFormation templates from the alarm configuration and output to the output/ directory.
     LONG
     method_option :application, aliases: :a, type: :string, desc: "application name"
     # method_option :validate, aliases: :v, type: :boolean, default: true, desc: "validate cfn templates"
@@ -48,24 +49,33 @@ module CfnMonitor
       CfnMonitor::Generate.run(options)
     end
 
-    desc "query", "Queries a cloudformation stack for monitorable resources"
+    desc "query", "Queries a CloudFormation stack for monitorable resources"
     long_desc <<-LONG
     This will provide a list of resources in the correct config syntax,
     including the nested stacks and the default templates for those resources.
     LONG
     method_option :application, aliases: :a, type: :string, desc: "application name"
-    method_option :stack, aliases: :s, type: :string, desc: "cfn stack name"
+    method_option :stack, aliases: :s, type: :string, desc: "CloudFormation stack name"
     def query
       CfnMonitor::Query.run(options)
     end
 
-    desc "deploy", "Deploys generated cfn templates to S3 bucket"
+    desc "deploy", "Deploys generated CloudFormation templates to an S3 bucket"
     long_desc <<-LONG
-    Deploys generated cloudformation templates to the specified S3 source_bucket
+    Deploys generated CloudFormation templates to the specified S3 source bucket
     LONG
     method_option :application, aliases: :a, type: :string, desc: "application name"
     def deploy
       CfnMonitor::Deploy.run(options)
+    end
+
+    desc "validate", "Validates CloudFormation templates in an S3 bucket"
+    long_desc <<-LONG
+    Validates generated CloudFormation templates in a specified S3 source bucket
+    LONG
+    method_option :application, aliases: :a, type: :string, desc: "application name"
+    def validate
+      CfnMonitor::Validate.run(options)
     end
 
   end
