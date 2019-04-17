df7ab960c1517074b6ed1e249c726eb3ea871a93
zac <z.lewis@base2services.com>
2019-04-17 12:10:47 +0200

    Add template for SQL checks.

--- lib/templates/sql.rb
+++ lib/templates/sql.rb
@@ -61,7 +61,7 @@ CloudFormation do
 
       Resource("SqlCheckFunction#{configHash}") do
         Type 'AWS::Lambda::Function'
-        Property('Code', { S3Bucket: FnJoin('.', ['base2.lambda', Ref('AWS::Region')]), S3Key: 'aws-lambda-sql-check/0.2/handler.zip' })
+        Property('Code', { S3Bucket: FnJoin('.', ['base2.lambda', Ref('AWS::Region')]), S3Key: 'aws-lambda-sql-check/0.1/handler.zip' })
         Property('Handler', 'main')
         Property('MemorySize', 128)
         Property('Runtime', 'go1.x')
