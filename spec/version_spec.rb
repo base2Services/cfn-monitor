0f186c2ee482cf195eb39f9a4069752c1beb3442
Guslington <guslington@gmail.com>
2019-11-12 14:58:08 +1100

    version bump to 0.4.3

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,7 +1,7 @@
 require 'cfn_monitor/version'
 
 describe 'Version' do
-  it 'is equal to 0.4.2' do
-    expect(CfnMonitor::VERSION).to eq("0.4.2")
+  it 'is equal to 0.4.3' do
+    expect(CfnMonitor::VERSION).to eq("0.4.3")
   end
 end
