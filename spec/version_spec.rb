f8f3138e4ea27ccc6eb1d57c402619f7e4caa2f2
Guslington <guslington@gmail.com>
2021-07-15 18:07:58 +1000

    update spec test

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,7 +1,7 @@
 require 'cfn_monitor/version'
 
 describe 'Version' do
-  it 'is equal to 0.4.4' do
-    expect(CfnMonitor::VERSION).to eq("0.4.4")
+  it 'is equal to 0.4.5' do
+    expect(CfnMonitor::VERSION).to eq("0.4.5")
   end
 end
